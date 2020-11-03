db73c04ea3afa7e876914cc3c78c8ca719cea031
Gavin Phill <carpe.gavin@gmail.com>
2020-11-03 14:29:14 -0600

    added quiz questions and answers to js

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+/*Which of the following function of String object returns the index within the calling String object of the last occurrence of the specified value?
+lastIndexOf()
+search()
+substr()
+indexOf()
+
+Which of the following function of Array object returns true if at least one element in this array satisfies the provided testing function?
+reverse()
+shift()
+slice()
+some()
+
+Which of the following is true about typeof operator in JavaScript?
+The typeOf is a unary operator that is placed before its single operand, which can be of any type
+It's value is a string indicating the data type of the operand
+Both of the above
+None of the above
+
+Executable single line of Script is called as
+Breakpoint in JS
+Statement in Javascript
+Line in JS
+None of the above
+
+Which of the following functions of Boolean objects return the primitive value of the Boolean object?
